7667fa97fb2bc2f8541c2971056a0357f404e426
YaIsrak <israkdreamstar@gmail.com>
2025-02-03 12:30:05 +0600

    loginpage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,25 @@
-
+import NavBrand from "@/components/layout/NavBrand";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Authentication",
+  title: "Authentication",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center">
-            {children}
-        </main>
-    );
+  return (
+    <main className="grid min-h-screen w-full grid-cols-1 md:grid-cols-2">
+      <div className="hidden flex-col items-start justify-center bg-primary text-white md:flex">
+        <div className="mx-auto space-y-2">
+          <NavBrand href="/" theme="dark" />
+          <h1 className="text-5xl font-bold">Welcome to Wink</h1>
+          <p>Login to continue</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">{children}</div>
+    </main>
+  );
 }
--- components/layout/NavBrand.tsx
+++ components/layout/NavBrand.tsx
@@ -1,16 +1,27 @@
-import logo from "@/public/logo-dark.png";
+import logoDark from "@/public/logo-dark.png";
+import logoLight from "@/public/logo-light.png";
 import Image from "next/image";
 import Link from "next/link";
 
 // todo: import fix
-export default function NavBrand({ href }: { href?: string }) {
+export default function NavBrand({
+  href,
+  theme = "light",
+}: {
+  href?: string;
+  theme?: "light" | "dark";
+}) {
   return (
     <Link
       href={href || "/explore"}
       className="flex items-center gap-2 text-3xl font-bold"
     >
       <div className="relative size-8">
-        <Image src={logo} placeholder="blur" alt="logo" fill />
+        {theme === "light" ? (
+          <Image src={logoDark} alt="logo" />
+        ) : (
+          <Image src={logoLight} alt="logo" />
+        )}
       </div>
       <span className="hidden font-mono md:block">Wink</span>
     </Link>
